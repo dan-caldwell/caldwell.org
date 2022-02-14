768cdf5e107a45d0764189930f081c0535a20a3d
dan-caldwell <dan@caldwell.info>
2022-02-13 22:48:22 -0500

    improve stock wizard

--- applications/stock-wizard/StockWizard.tsx
+++ applications/stock-wizard/StockWizard.tsx
@@ -5,6 +5,17 @@ import DataPointListItem from './components/DataPointListItem';
 const today = DateHelper.currentDate();
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+const formatObjectKeyTitle = (title: string) => {
+    return (title.match(/[A-Z][a-z]+|[0-9-]+/g) || title.split(' '))
+        .join(' ')
+        .replace('Peg', 'Price/Earnings-to-Growth')
+        .replace('Pe', 'Price/Earnings')
+        .replace('Pb', 'Price/Book')
+        .replace('Ps', 'Price/Sales')
+        .replace('Current', '')
+        .replace('Trailing', '');
+}
+
 const StockWizard = () => {
     const [dailyPick, setDailyPick] = useState({
         name: null,
@@ -44,6 +55,12 @@ const StockWizard = () => {
 
     const sellOnObj = dailyPick.sellOn ? new Date(dailyPick.sellOn) : new Date();
 
+    const currentAverages = Object.entries(spyAverages.average || {}).filter(item => item[0].includes('Current'));
+    const disallowedKeys = ['Current', 'ID', 'Name', 'SEDOL', 'Weight', 'Shares Held'];
+    const historicalAverages = Object.entries(spyAverages.average || {}).filter(item => {
+        return !disallowedKeys.find(key => item[0].includes(key))
+    });
+
     return (
         <>
             <div className="bg-white border border-gray-300 rounded-lg p-4 mb-4">
@@ -63,36 +80,28 @@ const StockWizard = () => {
                     S&P 500 averages for {today}
                 </div>
                 {spyAverages.average ? (
-                    <div className="text-left flex flex-col text-md">
-                        <DataPointListItem
-                            title="P/E ratio"
-                            value={spyAverages.average.PriceToEarningsRatio}
-                        />
-                        <DataPointListItem
-                            title="Yield"
-                            value={spyAverages.average.Yield}
-                        />
-                        <DataPointListItem
-                            title="% of float shorted"
-                            value={spyAverages.average.PercentOfFloatShorted}
-                        />
-                        <DataPointListItem
-                            title="Annualized dividend"
-                            value={spyAverages.average.AnnualizedDividend}
-                        />
-                        <DataPointListItem
-                            title="Estimated P/E next year"
-                            value={spyAverages.average.EstimatedPriceToEarningsRatioNextYear}
-                        />
-                        <DataPointListItem
-                            title="Sales per employee"
-                            value={spyAverages.average.SalesPerEmployee}
-                        />
-                        <DataPointListItem
-                            title="Beta"
-                            value={spyAverages.average.Beta}
-                        />
-                    </div>
+                    <>
+                        <div className="border-b mb-2 border-gray-300">Current</div>
+                        <div className="text-left flex flex-col text-md">
+                            {currentAverages.map(([key, value]) => {
+                                return <DataPointListItem
+                                    title={formatObjectKeyTitle(key)}
+                                    value={value}
+                                    key={key}
+                                />
+                            })}
+                        </div>
+                        <div className="mt-4 mb-2 border-b border-gray-300">Historical</div>
+                        <div className="text-left flex flex-col text-md">
+                            {historicalAverages.map(([key, value]) => {
+                                return <DataPointListItem
+                                    title={formatObjectKeyTitle(key)}
+                                    value={value}
+                                    key={key}
+                                />
+                            })}
+                        </div>
+                    </>
                 ) : (
                     <div>Loading...</div>
                 )}
--- applications/stock-wizard/api/classes/StockAPI.js
+++ applications/stock-wizard/api/classes/StockAPI.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const baseURL = `https://www.${process.env.STOCK_DATA_DOMAIN}/market-data`;
 
 class StockAPI {
 
@@ -7,15 +6,48 @@ class StockAPI {
         ticker
     }) {
         try {
-            const formattedTicker = ticker.toLowerCase();
-            const decodedURIComponent = `{"ticker":"${formattedTicker}","path":"/${formattedTicker}"}`;
-            const res = await axios.get(`${baseURL}/quotes/${formattedTicker.toUpperCase()}?id=${encodeURIComponent(decodedURIComponent)}&type=quotes_chart`, {});
-            return res.data.data.quoteData.Financials;
+            const unixTime = Math.round((new Date()).getTime() / 1000);
+            const formattedTicker = ticker.toUpperCase();
+            const res = await axios.get(`https://query2.${process.env.STOCK_DATA_DOMAIN}/ws/fundamentals-timeseries/v1/finance/timeseries/${formattedTicker}?lang=en-US&region=US&symbol=${formattedTicker}&padTimeSeries=true&type=quarterlyMarketCap%2CtrailingMarketCap%2CquarterlyEnterpriseValue%2CtrailingEnterpriseValue%2CquarterlyPeRatio%2CtrailingPeRatio%2CquarterlyForwardPeRatio%2CtrailingForwardPeRatio%2CquarterlyPegRatio%2CtrailingPegRatio%2CquarterlyPsRatio%2CtrailingPsRatio%2CquarterlyPbRatio%2CtrailingPbRatio%2CquarterlyEnterprisesValueRevenueRatio%2CtrailingEnterprisesValueRevenueRatio%2CquarterlyEnterprisesValueEBITDARatio%2CtrailingEnterprisesValueEBITDARatio&merge=false&period1=0&period2=${unixTime}&corsDomain=*`);
+            const {
+                data: {
+                    timeseries: {
+                        result
+                    }
+                }
+            } = res;
+            return Object.fromEntries(result.map(item => {
+                const out = [];
+                const types = item.meta.type;
+                const keys = Object.keys(item);
+                for (const key of keys) {
+                    if (!types.includes(key)) {
+                        delete item[key];
+                    } else if (item[key]) {
+                        // Sort by asOfDate
+                        item[key].sort((a, b) => {
+                            return new Date(a?.asOfDate).getTime() - new Date(b?.asOfDate).getTime()
+                        });
+
+                        // Get the values in the as-of date
+                        for (const [index, obj] of item[key].entries()) {
+                            const keyStart = key.charAt(0).toUpperCase() + key.slice(1);
+                            let keyName = keyStart + (obj?.asOfDate ? `_${obj.asOfDate}` : '');
+                            if (obj?.periodType === 'TTM' && index === item[key].length - 1) {
+                                keyName = keyStart + '_Current';
+                            }
+                            out.push([keyName, obj?.reportedValue?.raw]);
+                        }
+                    } else if (key !== 'error') {
+                        console.log(`No value for ${ticker} key ${key}`);
+                    }
+                }
+                return out;
+            }).flat());
         } catch (err) {
             console.error(err);
             return null;
         }
-
     }
 
 }
