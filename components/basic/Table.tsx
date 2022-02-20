3cafaac8ba458e9e62ccfdd2cff0617830a50b5c
dan-caldwell <dan@caldwell.info>
2022-02-19 22:11:16 -0500

    sorting of table items

--- components/basic/Table.tsx
+++ components/basic/Table.tsx
@@ -1,4 +1,15 @@
 import { useState } from "react";
+import clone from 'clone';
+
+const sortIcons = {
+    asc: '↑',
+    desc: '↓'
+}
+
+const sortParsers = {
+    int: (value: string) => parseInt(value.replace(/[A-Za-z%,]/g, '').replace(/−/g, '-')) || 0,
+    float: (value: string) => parseFloat(value.replace(/[A-Za-z%,]/g, '').replace(/−/g, '-')) || 0
+}
 
 const Table = ({
     headers: rawHeaders,
@@ -7,17 +18,38 @@ const Table = ({
 }) => {
     const [headers, setHeaders] = useState(rawHeaders);
     const [rows, setRows] = useState(rawRows);
+    const [sortBy, setSortBy] = useState({
+        type: 'asc',
+        index: 0
+    });
 
     const sortRows = (header: string, index: number) => {
-        const rowValues = rows.map(row => row[index]);
 
         if (headerSortKeys[header]) {
-            console.log({
-                header,
-                sorted: rowValues.sort((a, b) => {
-                    return headerSortKeys[header](b) - headerSortKeys[header](a)
-                })
-            })
+            const parseType = typeof headerSortKeys[header];
+            if (parseType !== 'function' && !sortParsers[headerSortKeys[header]]) return;
+            setRows((oldRows) => {
+                const newRows = clone(oldRows);
+                // Sort rows via the sort key function as defined in the props
+                newRows.sort((a, b) => {
+                    const larger = sortBy.type === 'asc' ? b : a;
+                    const smaller = sortBy.type === 'asc' ? a : b;
+                    if (parseType === 'function') return headerSortKeys[header](larger[index]) - headerSortKeys[header](smaller[index]);
+                    return sortParsers[headerSortKeys[header]](larger[index]) - sortParsers[headerSortKeys[header]](smaller[index])
+                });
+                return newRows;
+            });
+            // Set the sorted state
+            setSortBy(sortBy.type === 'asc' ?
+                {
+                    index,
+                    type: 'desc'
+                } :
+                {
+                    index,
+                    type: 'asc'
+                }
+            );
         }
     }
 
@@ -28,17 +60,17 @@ const Table = ({
                     className="sticky top-0 bg-white table-header-sticky"
                 >
                     {headers.map((header: string, index: number) => (
-                        <th 
-                            className="border-r border-gray-200 last:border-0 p-2"
+                        <th
+                            className={`border-r border-gray-200 last:border-0 p-2 ${headerSortKeys[header] ? 'cursor-pointer': ''}`}
                             onClick={() => sortRows(header, index)}
                             key={header}
-                        >{header}</th>
+                        >{header} {sortBy.index === index ? sortIcons[sortBy.type] : ''}</th>
                     ))}
                 </tr>
             </thead>
             <tbody>
                 {rows.map((row: string[]) => (
-                    <tr 
+                    <tr
                         className="border-b border-gray-200"
                         key={row.join('')}
                     >
--- pages/blog/data/city-index.tsx
+++ pages/blog/data/city-index.tsx
@@ -9,13 +9,18 @@ const Index = (props) => {
     const headers = props?.importedFiles[0]?.body?.headers || [];
     const rows = props?.importedFiles[0]?.body?.rows || [];
 
+    const generalFloatSort = (value: string) => parseFloat(value.replace(/[A-Za-z%,]/g, '').replace(/−/g, '-')) || 0;
+    const generalIntSort = (value: string) => parseInt(value.replace(/[A-Za-z%,]/g, '').replace(/−/g, '-')) || 0;
+
     return (
         <Post {...props}>
             <Table
                 headers={headers}
                 rows={rows}
                 headerSortKeys={{
-                    '2020 population density (mi)': (value: string) => parseInt(value.replace(/,/g, ''))
+                    '2020 population density (mi)': 'int',
+                    '2020 land area (mi)': 'float',
+                    'Change': 'float'
                 }}
             />
         </Post>
