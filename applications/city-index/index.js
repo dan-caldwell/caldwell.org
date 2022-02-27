7358adbe16ee38d9f17b3d1c8c84ae94709f11c0
dan-caldwell <dan@caldwell.info>
2022-02-27 15:50:46 -0500

    update wikipedia script

--- applications/city-index/index.js
+++ applications/city-index/index.js
@@ -8,56 +8,64 @@ const s3 = new AWS.S3({
 
 const bucket = 'caldwell-apps';
 
-// Get the data from the Wikipedia API
-axios.get(`https://en.wikipedia.org/w/api.php?action=parse&page=List_of_United_States_cities_by_population&section=1&prop=text&format=json`)
-    .then(async res => {
-        const {
-            data: {
-                parse: {
-                    text: {
-                        "*": html
-                    }
-                }
-            }
-        } = res;
-        const output = {
-            headers: [
-                '2020 rank',
-                'City',
-                'State',
-                '2020 census',
-                '2010 census',
-                'Change',
-                '2020 land area (mi)',
-                //'2020 land area (km)', 
-                '2020 population density (mi)',
-                //'2020 population density (km)', 
-                //'Location'
-            ],
-            rows: []
-        };
-        // Filter the rows
-        const excludedIndexes = [7, 9, 10];
-        const root = parse(html);
-        const table = root.querySelector('table.sortable');
-        const rows = table.querySelectorAll('tbody tr')
-            .map(row =>
-                row.childNodes
-                    .map(child =>
-                        child.innerText
-                            .replace('\n', '')
-                            .replace(/&#91;.*?&#93;/g, '') // Replace the brackets
-                    )
-                    .filter(child => child)
-                    .filter((child, index) => !excludedIndexes.includes(index))
-            ).slice(1);
-        output.rows = rows;
-
+(async () => {
+    try {
+        const output = await getWikipediaInfo();
         await s3.putObject({
             Bucket: bucket,
             Key: 'city-index/data.json',
             Body: JSON.stringify(output),
             ContentType: 'application/json'
         }).promise();
-    })
-    .catch(err => console.error(err));
+    } catch (err) {
+        console.error(err);
+    }
+
+})();
+
+const getWikipediaInfo = async () => {
+    // Get the data from the Wikipedia API
+    const wikiRes = await axios.get(`https://en.wikipedia.org/w/api.php?action=parse&page=List_of_United_States_cities_by_population&section=1&prop=text&format=json`);
+    const {
+        data: {
+            parse: {
+                text: {
+                    "*": html
+                }
+            }
+        }
+    } = wikiRes;
+    const output = {
+        headers: [
+            '2020 rank',
+            'City',
+            'State',
+            '2020 census',
+            '2010 census',
+            'Change',
+            '2020 land area (mi)',
+            //'2020 land area (km)', 
+            '2020 population density (mi)',
+            //'2020 population density (km)', 
+            //'Location'
+        ],
+        rows: []
+    };
+    // Filter the rows
+    const excludedIndexes = [7, 9, 10];
+    const root = parse(html);
+    const table = root.querySelector('table.sortable');
+    const rows = table.querySelectorAll('tbody tr')
+        .map(row =>
+            row.childNodes
+                .map(child =>
+                    child.innerText
+                        .replace('\n', '')
+                        .replace(/&#91;.*?&#93;/g, '') // Replace the brackets
+                )
+                .filter(child => child)
+                .filter((child, index) => !excludedIndexes.includes(index))
+        ).slice(1);
+    output.rows = rows;
+    return output;
+}
